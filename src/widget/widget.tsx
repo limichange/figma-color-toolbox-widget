7131da58e370caeb87a39f5cd84bf2409b873260
limichange <limichange@hotmail.com>
2022-08-01 16:07:23 +0800

    feat: remove old color layout

--- src/widget/widget.tsx
+++ src/widget/widget.tsx
@@ -11,25 +11,22 @@ import { horizontalSmallLayoutIcon } from './images/horizontalSmallLayoutIcon'
 import { updateIcon } from './images/updateIcon'
 import { verticalLargeLayoutIcon } from './images/verticalLargeLayoutIcon'
 import { verticalSmallLayoutIcon } from './images/verticalSmallLayoutIcon'
-import { svgSrc, svgSrc2 } from './svg'
 
 const { widget } = figma
 const {
   useEffect,
-  SVG,
   AutoLayout,
   waitForTask,
   usePropertyMenu,
   useSyncedState,
   Text,
-  Ellipse,
   Input,
 } = widget
 const h = figma.widget.h
 
 function Widget() {
   const [styleName, setStyleName] = useSyncedState('styleName', '')
-  const [hue, setHue] = useSyncedState('hue', 0)
+  const [, setHue] = useSyncedState('hue', 0)
   const [color, setColor] = useSyncedState('color', {
     r: 255,
     g: 0,
@@ -37,23 +34,8 @@ function Widget() {
     a: 1,
   })
 
-  const [pointerPostion, setPointerPostion] = useSyncedState('pointerPostion', {
-    x: 0,
-    y: 0,
-  })
-
-  const [backgroundColor, setBackgroundColor] = useSyncedState(
-    'backgroundColor',
-    ''
-  )
+  const [, setBackgroundColor] = useSyncedState('backgroundColor', '')
   const [colorFormat, setColorFormat] = useSyncedState('colorFormat', 'Hex')
-
-  // const formatOptions = [
-  //   { option: 'Hex', label: 'Hex' },
-  //   { option: 'RGB', label: 'RGB' },
-  //   { option: 'HSL', label: 'HSL' },
-  //   { option: 'HSV', label: 'HSV' },
-  // ]
 
   const menu: WidgetPropertyMenuItem[] = [
     {
@@ -92,23 +74,11 @@ function Widget() {
       propertyName: 'update',
       icon: updateIcon,
     },
-    // {
-    //   itemType: 'dropdown',
-    //   propertyName: 'update color format',
-    //   tooltip: 'Format',
-    //   selectedOption: colorFormat,
-    //   options: formatOptions,
-    // },
   ]
 
   function updateWidgetColor(color: RgbaColor) {
     const hsva = rgbaToHsva(color)
     setColor(color)
-
-    setPointerPostion({
-      x: hsva.s / 100,
-      y: 1 - hsva.v / 100,
-    })
 
     const bg = hsvaToHex({ h: hsva.h, s: 100, v: 100, a: 1 })
 
@@ -238,45 +208,6 @@ function Widget() {
         direction: 'vertical',
         spacing: 6,
       },
-      h(
-        AutoLayout,
-        {
-          fill: backgroundColor,
-        },
-        h(SVG, {
-          src: svgSrc,
-          width: 256,
-          height: 256,
-        }),
-        h(Ellipse, {
-          width: 16,
-          height: 16,
-          positioning: 'absolute',
-          x: pointerPostion.x * 256 - 8,
-          y: pointerPostion.y * 256 - 8,
-          stroke: '#ffffff',
-          strokeWidth: 2,
-        })
-      ),
-      h(
-        AutoLayout,
-        {},
-        h(SVG, {
-          src: svgSrc2,
-          width: 256,
-          height: 20,
-        }),
-        h(Ellipse, {
-          width: 16,
-          height: 16,
-          positioning: 'absolute',
-          x: (hue / 360) * 256 - 8,
-          y: 2,
-          fill: backgroundColor,
-          stroke: '#ffffff',
-          strokeWidth: 2,
-        })
-      ),
       colorFormat === 'RGB' && h(Text, {}, hsvaToRgbaString(rgbaToHsva(color))),
       colorFormat === 'Hex' && h(Text, {}, hsvaToHex(rgbaToHsva(color))),
       colorFormat === 'HSL' && h(Text, {}, hsvaToHslaString(rgbaToHsva(color))),
